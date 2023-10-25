49f3b08f0fae3cdeb5c7845e83e2290954e4d631
surajverma2587 <surajverma2587@gmail.com>
2023-10-25 20:25:30 +0100

    wip

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,5 +1,42 @@
 import ListGroup from "react-bootstrap/ListGroup";
+import Card from "react-bootstrap/Card";
+import Stack from "react-bootstrap/Stack";
+import Button from "react-bootstrap/Button";
 
 export const TodoItem = ({ todoItem }) => {
-  return <ListGroup.Item as="li">{todoItem.title}</ListGroup.Item>;
+  return (
+    <ListGroup.Item as="li">
+      <Card>
+        <Card.Body>
+          <Card.Title>{todoItem.title}</Card.Title>
+          <ListGroup className="list-group-flush">
+            <ListGroup.Item>
+              <Stack direction="horizontal" className="justify-content-between">
+                <div>Status</div>
+                <div>{todoItem.status}</div>
+              </Stack>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Stack direction="horizontal" className="justify-content-between">
+                <div>Created at</div>
+                <div>{todoItem.createdAt}</div>
+              </Stack>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Stack direction="horizontal" className="justify-content-between">
+                <div>Updated at</div>
+                <div>{todoItem.updatedAt}</div>
+              </Stack>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card.Body>
+        <Card.Footer>
+          <Stack direction="horizontal" className="justify-content-evenly">
+            <Button variant="warning">Edit</Button>
+            <Button variant="danger">Delete</Button>
+          </Stack>
+        </Card.Footer>
+      </Card>
+    </ListGroup.Item>
+  );
 };
